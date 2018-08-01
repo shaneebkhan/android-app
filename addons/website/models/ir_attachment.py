e9b7f7eb95d143df35b2dfdbc69591e5376b71af
Jeremy Kersten <jke@odoo.com>
2018-08-01 11:10:42 +0200

    remove_theme_id_from_Attachment

--- addons/website/models/ir_attachment.py
+++ addons/website/models/ir_attachment.py
@@ -3,8 +3,12 @@
 
 import logging
 from odoo import api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
+
 
 _logger = logging.getLogger(__name__)
+
 
 class Attachment(models.Model):
 
@@ -13,7 +17,7 @@ class Attachment(models.Model):
     # related for backward compatibility with saas-6
     website_url = fields.Char(string="Website URL", related='local_url', deprecated=True)
     key = fields.Char(help='Technical field used to resolve multiple attachments in a multi-website environment.')
-    theme_id = fields.Many2one('ir.module.module')
+    website_id = fields.Many2one('website')
 
     @api.multi
     def unlink(self):
@@ -24,20 +28,24 @@ class Attachment(models.Model):
     def _get_theme_specific_attachment(self, theme_name):
         self.ensure_one()
         attachment = self
-        module_being_updated = self.env['ir.module.module'].search([('name', '=', theme_name)])
+        current_website = self._context.get('website_id')
+
+        if not current_website:
+            raise UserError(_('Cannot update theme without website'))
+
         xml_id = self.env['ir.model.data'].search([('model', '=', 'ir.attachment'), ('res_id', '=', attachment.id)])
         if xml_id and xml_id.module != theme_name:
             _logger.info('%s is updating attachment %s (ID: %s)', theme_name, xml_id.complete_name, attachment.id)
 
             # check if a previously copied attachment for this theme already exists
-            theme_specific_attachment = self.env['ir.attachment'].search([('key', '=', attachment.key), ('theme_id', '=', module_being_updated.id)])
+            theme_specific_attachment = self.env['ir.attachment'].search([('key', '=', attachment.key), ('website_id', '=', current_website)])
             if theme_specific_attachment:
                 attachment = theme_specific_attachment
                 _logger.info('diverting write to %s (ID: %s)', attachment.name, attachment.id)
             else:
                 attachment.with_context(no_cow=True).key = xml_id.complete_name
                 attachment = attachment.copy({
-                    'theme_id': module_being_updated.id,
+                    'website_id': current_website,
                     'key': xml_id.complete_name,
                 })
                 _logger.info('created new theme-specific attachment %s (ID: %s)', attachment.name, attachment.id)
@@ -46,13 +54,11 @@ class Attachment(models.Model):
 
     @api.multi
     def write(self, vals):
-        if not self._context.get('no_cow'):
-            for attachment in self:
-                currently_updating = self._context.get('install_mode_data', {}).get('module', '')
-                if currently_updating.startswith('theme_'):
-                    attachment = attachment._get_theme_specific_attachment(currently_updating)
-
-                super(Attachment, attachment).write(vals)
+        currently_updating = self._context.get('install_mode_data', {}).get('module', '')
+        if not self._context.get('no_cow') and currently_updating.startswith('theme_'):
+            for attach in self:
+                attach = attach._get_theme_specific_attachment(currently_updating)
+                super(Attachment, attach).write(vals)
         else:
             super(Attachment, self).write(vals)
 
