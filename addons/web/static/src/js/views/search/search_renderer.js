69664b0ffb23b3e9c93eee1dd2b77e3c5c4c9ff1
Mathieu Duckerts-Antoine <dam@odoo.com>
2018-10-23 17:02:40 +0200

    basic render method and template

--- addons/web/static/src/js/views/search/search_renderer.js
+++ addons/web/static/src/js/views/search/search_renderer.js
@@ -2,9 +2,37 @@ odoo.define('web.SearchRenderer', function (require) {
 "use strict";
 
 var AbstractRenderer = require('web.AbstractRenderer');
+var AutoComplete = require('web.AutoComplete');
 
 var SearchRenderer = AbstractRenderer.extend({
-	className: 'o_search_renderer'
+	template: 'SearchView',
+
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    _render: function () {
+    	var defs = [];
+    	defs.push(this._setupAutoCompletion());
+    	return $.when(this, defs);
+    },
+    /**
+     * instantiate auto-completion widget
+     */
+    _setupAutoCompletion: function () {
+        this.autoComplete = new AutoComplete(this, {
+        	// the widget should be changed
+        	// I have changed source and select for now because I don't want to
+        	// break things
+            source: function () {},
+            select: function () {},
+            get_search_string: function () {
+                return this.$('.o_searchview_input').val().trim();
+            },
+        });
+        return this.autoComplete.appendTo(this.$('.o_searchview_input_container'));
+    },
 });
 
 return SearchRenderer;
