1b1985132d73b3fd59247da0347e5d5c82279411
Martin Geubelle <mge@odoo.com>
2018-10-30 14:24:19 +0100

    [IMP] searchbar: display option in facet

--- addons/web/static/src/js/views/search/search_facet.js
+++ addons/web/static/src/js/views/search/search_facet.js
@@ -16,13 +16,19 @@ var SearchFacet = Widget.extend({
     init: function (parent, facet) {
         this._super.apply(this, arguments);
 
+        var self = this;
         this.facet = facet;
-        this.facetValues = _.map(this.facet.values, function (value) {
-            return value.description;
+        this.facetValues = _.map(this.facet.values, function (atom) {
+            return self._getAtomDescription(atom);
         });
         this.separator = this._getSeparator();
         this.icon = this._getIcon();
     },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
     _getIcon: function () {
         var icon;
         if (this.facet.type === 'filter') {
@@ -41,6 +47,21 @@ var SearchFacet = Widget.extend({
         }
         return separator;
     },
+    _getAtomDescription: function (atom) {
+        var description = atom.description;
+        if (atom.hasOptions) {
+            var optionValue =_.findWhere(atom.options, {
+                optionId: atom.currentOptionId,
+            });
+            description += ': ' + optionValue.description;
+        }
+        return description;
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
     _onFacetRemove: function () {
         this.trigger_up('facet_removed', this.facet);
     },
