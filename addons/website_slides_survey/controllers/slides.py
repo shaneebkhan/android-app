5290a8ead31404a15d1c99a645c7075020e3eac0
qmo-odoo <qmo@odoo.com>
2019-03-27 10:16:53 +0100

    [IMP] website_slides: Improve methods to get user certificates
    This commit improves a few methods related to the user profiles:
     - Change _prepare_all_users_values so that it
       works on batch
     - Change the _get_user_certificates to avoid having
       a search in a loop
     - Introduce a new method that will only get the number
       of certifications per user

--- addons/website_slides_survey/controllers/slides.py
+++ addons/website_slides_survey/controllers/slides.py
@@ -10,6 +10,7 @@ from odoo import http
 from odoo.http import request
 
 from odoo.addons.website_slides.controllers.main import WebsiteSlides
+from collections import defaultdict
 
 
 class WebsiteSlides(WebsiteSlides):
@@ -44,24 +45,51 @@ class WebsiteSlides(WebsiteSlides):
     def _prepare_user_slides_profile(self, user):
         values = super(WebsiteSlides, self)._prepare_user_slides_profile(user)
         values.update({
-            'certificates': self._get_user_certificates(user),
+            'certificates': self._get_user_certificates(user)[user.id]
         })
         return values
 
     # All Users Page
     # ---------------------------------------------------
-    def _prepare_all_users_values(self, user):
-        result = super(WebsiteSlides, self)._prepare_all_users_values(user)
-        result.update({
-            'certification_count': len(self._get_user_certificates(user))
-        })
+    def _prepare_all_users_values(self, users):
+        result = super(WebsiteSlides, self)._prepare_all_users_values(users)
+        certificates = self._get_user_certificates_count(users)
+        for index, user in enumerate(users):
+            result[index].update({
+                'certification_count': certificates[user.partner_id.id] if certificates.get(user.partner_id.id) else None
+            })
         return result
 
-    def _get_user_certificates(self, user):
+    def _get_user_certificates_count(self, users):
+        partner_ids = [user.partner_id.id for user in users]
         domain = [
-            ('partner_id', '=', user.partner_id.id),
+            ('partner_id', 'in', partner_ids),
+            ('quizz_passed', '=', True),
+            ('slide_partner_id.survey_quizz_passed', '=', True)
+        ]
+        certifications_data = request.env['survey.user_input'].sudo().read_group(
+            domain,
+            fields=['partner_id'],
+            groupby=['partner_id']
+        )
+        users_certifications_count = {datum['partner_id'][0]: datum['partner_id_count'] for datum in certifications_data}
+        return users_certifications_count
+
+    def _get_user_certificates(self, users):
+        partner_ids = [user.partner_id.id for user in users]
+        domain = [
+            ('partner_id', 'in', partner_ids),
             ('quizz_passed', '=', True),
             ('slide_partner_id.survey_quizz_passed', '=', True)
         ]
         certifications = request.env['survey.user_input'].sudo().search(domain)
-        return certifications
+        users_certifications = {
+            user.id: [
+                {
+                    'survey_id': certification.survey_id,
+                    'slide_id': certification.slide_id,
+                    'quizz_score': certification.quizz_score
+                } for certification in certifications if certification.partner_id == user.partner_id
+            ] for user in users
+        }
+        return users_certifications
