82a964bd1468f020214983ac2b6d41bb6b0a964c
Aaron Bohy <aab@odoo.com>
2019-04-29 14:02:49 +0200

    [IMP] createView helper: use custom search_view
    This commit offers the possibility to use a
    custom search view when creating a view
    via the test_utils_create createView function.

--- addons/web/static/tests/helpers/test_utils_create.js
+++ addons/web/static/tests/helpers/test_utils_create.js
@@ -154,6 +154,12 @@ async function createView(params) {
             modelName: params.model || 'foo',
         });
     } else {
+        viewOptions.controlPanelFieldsView = testUtilsMock.fieldsViewGet(mockServer, {
+            arch: params.archs && params.archs[params.model + ',false,search'] || '<search/>',
+            fields: viewInfo.fields,
+            model: params.model,
+        });
+
         view = new params.View(viewInfo, viewOptions);
     }
 
