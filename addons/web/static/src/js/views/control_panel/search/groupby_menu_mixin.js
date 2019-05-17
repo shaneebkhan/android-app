6de99c6974c22fb1e78b4ef877a13d1c570d10f9
Thomas Werland <twe@odoo.com>
2019-05-17 12:14:39 +0200

    [IMP] Modified the style of the groupby button when it's embedded into the dashboard view

--- addons/web/static/src/js/views/control_panel/search/groupby_menu_mixin.js
+++ addons/web/static/src/js/views/control_panel/search/groupby_menu_mixin.js
@@ -44,7 +44,7 @@ var GroupByMenuMixin = {
         this.groupableFields = groupableFields;
         var groupBys = this._getGroupBys(this.model.get().groupBy);
         this.groupByMenu = new GroupByMenu(this, groupBys, this.groupableFields);
-        this.groupByMenu.insertAfter($node.find('div:first'));
+        return this.groupByMenu.insertAfter($node.find('div:first'));
     },
     /**
      * This method puts the active groupBys in a convenient form.
