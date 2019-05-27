kaofelatrader/odoo

[IMP] account,point_of_sale: robustness
prevent editing in pos_config with active session:
    active
    module_pos_restaurant
    journal_ids
    sequence_ids
    pickint_type_id
    journal_id

prevent deletion and deactivation of models used
in pos session:
    account_account
    account_journal
    ir_sequence
    product_template
    res_partner
    res_users
    stock_picking_type

If locally stored session information does not match with the available
database information, show a popup windows explaining there is a
conflict in the data at hand. The popup gives the user the oportunity to
save the local storage data and clear the local storage so they can
continue selling.

Task id: 1879971

addons/point_of_sale/models/ir_sequence.py
@@ -1,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    @api.multi
+    def write(self, vals):
+        """ Prevent archiving the sequences that are still used in opened sessions """
+        if not vals.get('active', True):
+            self._check_pos_sessions(_("You cannot archive a sequence that is used in a PoS session, close the session(s) first: \n"))
+        return super(IrSequence, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        """ Prevent removing the sequences that are still used in opened sessions """
+        self._check_pos_sessions(_("You cannot remove a sequence that is used in a PoS session, close the session(s) first: \n"))
+
+        return super(IrSequence, self).unlink()
+
+    def _check_pos_sessions(self, error_msg):
+        confs = self.env['pos.session'].search([
+            ('state', '!=', 'closed'),
+            ('config_id.sequence_id', 'in', self.ids),
+        ]).mappes('config_id')
+        if confs:
+            # find the problematic sequence back from the one in self
+            sequences = confs.mapped('sequence_id')
+            sequences = sequences & self
+
+            for sequence in sequences:
+                configs = [config for config in confs
+                        if sequence == config.sequence_id]
+                error_msg += _("Sequence: %s - PoS Config(s): %s \n") % (sequence.name, ', '.join(config.name for config in configs))
+            raise UserError(error_msg)

shaneebkhan/android-app

addons/point_of_sale/models/ir_sequence.py
@@ -1,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    @api.multi
+    def write(self, vals):
+        """ Prevent archiving the sequences that are still used in opened sessions """
+        if not vals.get('active', True):
+            self._check_pos_sessions(_("You cannot archive a sequence that is used in a PoS session, close the session(s) first: \n"))
+        return super(IrSequence, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        """ Prevent removing the sequences that are still used in opened sessions """
+        self._check_pos_sessions(_("You cannot remove a sequence that is used in a PoS session, close the session(s) first: \n"))
+
+        return super(IrSequence, self).unlink()
+
+    def _check_pos_sessions(self, error_msg):
+        confs = self.env['pos.session'].search([
+            ('state', '!=', 'closed'),
+            ('config_id.sequence_id', 'in', self.ids),
+        ]).mapped('config_id')
+        if confs:
+            # find the problematic sequence back from the one in self
+            sequences = confs.mapped('sequence_id')
+            sequences = sequences & self
+
+            for sequence in sequences:
+                configs = [config for config in confs
+                        if sequence == config.sequence_id]
+                error_msg += _("Sequence: %s - PoS Config(s): %s \n") % (sequence.name, ', '.join(config.name for config in configs))
+            raise UserError(error_msg)