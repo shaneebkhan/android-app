a2ebd1780b781da14e98250b69473b811e2b3014
Kevin Baptiste <kba@odoo.com>
2019-05-29 16:40:00 +0200

    [IMP] hr: widget for background zoomable image

--- addons/hr/static/src/js/image_field.js
+++ addons/hr/static/src/js/image_field.js
@@ -1,15 +1,16 @@
 odoo.define('hr.fields', function (require) {
     "use strict";
 
+    var Widget = require('web.Widget');
     var basic_fields = require('web.basic_fields');
     var core = require('web.core');
     var registry = require('web.field_registry');
     var session = require('web.session');
     var field_utils = require('web.field_utils');
 
-
     require("website.content.zoomodoo");
 
+    var qweb = core.qweb;
     var _lt = core._lt;
 
     var FieldImageWithZoom = basic_fields.FieldBinaryImage.extend({
@@ -30,15 +31,58 @@ odoo.define('hr.fields', function (require) {
 
                 $img.attr('data-zoom', 1);
                 $img.attr('data-zoom-image', url);
-                
+
                 $img.zoomOdoo({ event: 'mouseenter', attach: '.o_action_manager' });
             }
         }
     });
 
+    var BackgroundImage = Widget.extend({
+        tagName: 'span',
+
+        init: function (parent, name, record, options) {
+            this._super.apply(this, arguments);
+
+            this.record = record;
+            this.options = options || {};
+
+            if('tag' in options.attrs)
+                this.tagName = options.attrs['tag'];
+
+            console.log('init');
+        },
+        start: function () {
+            console.log('start');
+            this._render();
+        },
+        _render: function() {
+            if(this.options && 'class' in this.options.attrs)
+                this.$el.addClass(this.options.attrs['class']);
+
+            var url = session.url('/web/image', {
+                model: this.record.model,
+                id: JSON.stringify(this.record.res_id),
+                field: 'image',
+            });
+            var url_thumb = session.url('/web/image', {
+                model: this.record.model,
+                id: JSON.stringify(this.record.res_id),
+                field: 'image_medium',
+            });
+
+            this.$el.css('backgroundImage', 'url(' + url_thumb + ')');
+            this.$el.attr('data-zoom', 1);
+            this.$el.attr('data-zoom-image', url);
+
+            this.$el.zoomOdoo({ event: 'mouseenter', attach: '.o_action_manager' });
+        }
+    });
+
     registry.add('image_zoom', FieldImageWithZoom);
+    registry.add('bgimage', BackgroundImage);
 
     return {
-        FieldImageWithZoom: FieldImageWithZoom
+        FieldImageWithZoom: FieldImageWithZoom,
+        BackgroundImage: BackgroundImage
     }
 });
