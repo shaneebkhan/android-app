894e780001ffd9a5fdc427bce07a4cd2386f5c92
David Monjoie <dmo@odoo.com>
2019-06-05 16:27:32 +0200

    [FIX] web: add view id in optional fields local storage key
    Since the renderer does not know the view id, we moved the local
    storage key computation up to the basic controller, which makes
    more sense since the renderer is supposed to be inert.
    
    The _processColumns function had to be moved in willStart however,
    since it now needs the controller to respond to the trigger_up
    and the controller is not present during the init of the renderer.

--- addons/web/static/tests/views/list_tests.js
+++ addons/web/static/tests/views/list_tests.js
@@ -6235,7 +6235,7 @@ QUnit.module('Views', {
         list.destroy();
     });
 
-    QUnit.test('optinal fields do not disappear even after listview reload', async function (assert) {
+    QUnit.test('optional fields do not disappear even after listview reload', async function (assert) {
         assert.expect(7);
 
         var RamStorageService = AbstractStorageService.extend({
@@ -6434,9 +6434,12 @@ QUnit.module('Views', {
             services: {
                 local_storage: RamStorageService,
             },
+            view_id: 42,
         });
 
-        assert.verifySteps(['getItem list_optional_fields,foo,list,foo:char,m2o:many2one,reference:reference']);
+        var localStorageKey = 'optional_fields,foo,list,42,foo,m2o,reference';
+
+        assert.verifySteps(['getItem ' + localStorageKey]);
 
         assert.containsN(list, 'th', 3,
             "should have 3 th, 1 for selector, 2 for columns");
@@ -6457,8 +6460,8 @@ QUnit.module('Views', {
         await testUtils.dom.click(list.$('div.o_optional_columns div.dropdown-item:eq(1) input'));
 
         assert.verifySteps([
-            'setItem list_optional_fields,foo,list,foo:char,m2o:many2one,reference:reference to ["m2o","reference"]',
-            'getItem list_optional_fields,foo,list,foo:char,m2o:many2one,reference:reference',
+            'setItem ' + localStorageKey + ' to ["m2o","reference"]',
+            'getItem ' + localStorageKey,
         ]);
 
         // 4 th (1 for checkbox, 3 for columns)
