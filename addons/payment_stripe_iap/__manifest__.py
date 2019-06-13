e28043c2837b969d9293600d7d55520d1ea16b7c
Hiral Bhavsar <hbh@odoo.com>
2019-06-13 15:18:46 +0530

    [ADD] payment_strip_iap: to create a standard stripe account

---
+++ addons/payment_stripe_iap/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Connect Stripe Account',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: Stripe Connect Implementation',
+    'version': '1.0',
+    'description': """Create a stripe standard account""",
+    'depends': ['iap'],
+    'data': [],
+    'installable': True,
+}
