c98fec242d1b34e958f4653de8c94c65d496f0dc
Florent Lejoly <fle@odoo.com>
2019-06-14 13:52:40 +0200

    wip

--- addons/crm_iap_lead_enrich/models/crm_iap_lead_enrich.py
+++ addons/crm_iap_lead_enrich/models/crm_iap_lead_enrich.py
@@ -33,7 +33,7 @@ class Lead(models.Model):
     
     @api.model
     def enrich_with_cron(self):
-        leads = self.search([('enriched_lead', '=', False),('reveal_id', '=', False),('won_status','=','pending')])
+        leads = self.search([('enriched_lead', '=', False),('reveal_id', '=', False)])
         leads.lead_enrich_mail()
 
     @api.model
