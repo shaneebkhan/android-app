98e8988d50fce9b5765c342379bb7c8221494451
Arnold Moyaux <arm@odoo.com>
2019-06-18 13:39:26 +0200

    [IMP] stock, stock_account: inventory report

--- addons/stock_account/wizard/stock_quantity_history.py
+++ addons/stock_account/wizard/stock_quantity_history.py
@@ -1,19 +1,20 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, _
+from odoo import fields, models
 
 
 class StockQuantityHistory(models.TransientModel):
     _inherit = 'stock.quantity.history'
 
-    def open_table(self):
-        if not self.env.context.get('valuation'):
-            return super(StockQuantityHistory, self).open_table()
+    def open_at_date(self):
+        active_model = self.env.context.get('active_model')
+        if active_model == 'stock.valuation.layer':
+            action = self.env.ref('stock_account.stock_valuation_layer_action').read()[0]
+            if self.date.date() != fields.Date.today():
+                action['domain'] = [('create_date', '<=', self.date)]
+                action['display_name'] = str(self.date)
+                return action
+            else:
+                return action
 
-        action = self.env.ref('stock_account.stock_valuation_layer_action').read()[0]
-        if int(self.compute_at_date):
-            action['domain'] = [('create_date', '<=', self.date)]
-            return action
-        else:
-            return action
-
+        return super(StockQuantityHistory, self).open_at_date()
