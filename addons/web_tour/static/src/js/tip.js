9d19b60397cb228df23d572bda13ebd695e1a3aa
Nicolas Martinelli <nim@odoo.com>
2019-06-25 13:40:59 +0200

    [FIX] web_tour:avoid unnecessary traceback
    This seems to introduce race conditions, so revert for the time being.
    
    This reverts commit 5bb9a8b4189eaa4cb5ed2ec10c752a9ee2993bfe.

--- addons/web_tour/static/src/js/tip.js
+++ addons/web_tour/static/src/js/tip.js
@@ -146,7 +146,6 @@ var Tip = Widget.extend({
     },
     _reposition: function () {
         if (this.tip_opened) return;
-        if (!this.$el) return;
         this.$el.removeClass("o_animated");
 
         // Reverse left/right position if direction is right to left
